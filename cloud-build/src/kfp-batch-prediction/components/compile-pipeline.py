024f42de3c54ad4785c0821f988a78e35a857728
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-22 20:53:01 -0500

    Add files via upload

---
+++ cloud-build/src/kfp-batch-prediction/components/compile-pipeline.py
@@ -0,0 +1,9 @@
+import kfp
+from kfp.v2 import dsl,compiler
+from kfp.v2.dsl import component,pipeline,Output,Artifact,Input,Model
+
+from make_pipeline import mlopsDeployPipeline
+
+compiler.Compiler().compile(pipeline_func = mlopsDeployPipeline,
+							package_path  = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/kfp-json/mlopsDeployPipeline-1.json'
+						   )
