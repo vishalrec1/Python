8649c322670de699fe4846077aba1215676adc61
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-22 21:30:57 -0500

    Update loadBigQueryTableFromGCS.py

--- cloud-build/src/kfp-batch-prediction/components/loadBigQueryTableFromGCS.py
+++ cloud-build/src/kfp-batch-prediction/components/loadBigQueryTableFromGCS.py
@@ -8,8 +8,8 @@ CUSTOM_SERVING_CONTAINER  = "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.
            #output_component_file = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/yaml/loadBigQueryTableFromGCS.yaml'
            output_component_file = '/loadBigQueryTableFromGCS.yaml'
           )
-def loadBigQueryTableFromGCS(PROJECT_ID,REGION,BUCKET_URI):
-	client = bigquery.Client(project=PROJECT_ID,location=REGION)
+def loadBigQueryTableFromGCS(project_in:str,bucket_uri_in:str):
+	client = bigquery.Client(project=project_in,location='us-central1')
 	job_config = bigquery.LoadJobConfig(
 										schema=[
 											bigquery.SchemaField("longitude",        "FLOAT"),
@@ -26,9 +26,9 @@ def loadBigQueryTableFromGCS(PROJECT_ID,REGION,BUCKET_URI):
 										# The source format defaults to CSV, so the line below is optional.
 										source_format = bigquery.SourceFormat.CSV,
 									)
-	uri      = BUCKET_URI+'/california_housing_train.csv'
+	uri      = bucket_uri_in+'/california_housing_train.csv'
 	table_id = 'gcp-practice-0123.dataset2.california_housing_test'
 	load_job = client.load_table_from_uri(source_uris = uri,
-										  destination = table_id,
-										  job_config  = job_config)  # Make an API request.
+					      destination = table_id,
+					      job_config  = job_config)  # Make an API request.
 	load_job.result()
