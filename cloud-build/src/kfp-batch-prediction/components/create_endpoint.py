6ecec67b1624aaca67acebe8913f69fb0f9fb8d5
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-22 21:10:50 -0500

    Update create_endpoint.py

--- cloud-build/src/kfp-batch-prediction/components/create_endpoint.py
+++ cloud-build/src/kfp-batch-prediction/components/create_endpoint.py
@@ -5,8 +5,8 @@ from kfp.v2.dsl import component,pipeline,Output,Artifact,Input,Model
 CUSTOM_SERVING_CONTAINER  = "us-central1-docker.pkg.dev/nonprod-corp-1cdh-214e/containers/wf_1cdh_notebook:r6"
 
 @component(base_image            = CUSTOM_SERVING_CONTAINER,
-           output_component_file = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/yaml/create_endpoint.yaml'
-           #output_component_file = '/home/jupyter/KFP-json2/yaml/create_endpoint.yaml'
+           #output_component_file = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/yaml/create_endpoint.yaml'
+           output_component_file = '/create_endpoint.yaml'
           )
 def create_endpoint(endpoint_display_name_in:str, 
                      project_in:str
