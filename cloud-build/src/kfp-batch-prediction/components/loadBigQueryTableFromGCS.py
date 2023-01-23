1727561a17a42efe5af8efa8d90ec97f4b1c4b78
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-22 20:50:15 -0500

    Add files via upload

---
+++ cloud-build/src/kfp-batch-prediction/components/loadBigQueryTableFromGCS.py
@@ -0,0 +1,34 @@
+from google.cloud import bigquery
+import kfp
+from kfp.v2 import dsl,compiler
+from kfp.v2.dsl import component,pipeline,Output,Artifact,Input,Model
+
+CUSTOM_SERVING_CONTAINER  = "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-0:latest"
+@component(base_image            = CUSTOM_SERVING_CONTAINER,
+           output_component_file = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/yaml/batch_prediction.yaml'
+           #output_component_file = '/home/jupyter/KFP-json2/yaml/batch_prediction.yaml'
+          )
+def loadBigQueryTableFromGCS(PROJECT_ID,REGION,BUCKET_URI):
+	client = bigquery.Client(project=PROJECT_ID,location=REGION)
+	job_config = bigquery.LoadJobConfig(
+										schema=[
+											bigquery.SchemaField("longitude",        "FLOAT"),
+											bigquery.SchemaField("latitude",      "FLOAT"),
+											bigquery.SchemaField("housing_median_age",  "IFLOAT"),
+											bigquery.SchemaField("total_rooms",      "FLOAT"),
+											bigquery.SchemaField("total_bedrooms", "FLOAT"),
+											bigquery.SchemaField("population",      "FLOAT"),
+											bigquery.SchemaField("households",      "FLOAT"),
+											bigquery.SchemaField("median_income",      "FLOAT"),
+											bigquery.SchemaField("median_house_value",      "FLOAT")
+										],
+										skip_leading_rows = 1,
+										# The source format defaults to CSV, so the line below is optional.
+										source_format = bigquery.SourceFormat.CSV,
+									)
+	uri      = BUCKET_URI+'/california_housing_train.csv'
+	table_id = 'gcp-practice-0123.dataset2.california_housing_test'
+	load_job = client.load_table_from_uri(source_uris = uri,
+										  destination = table_id,
+										  job_config  = job_config)  # Make an API request.
+	load_job.result()
---
+++ cloud-build/src/kfp-batch-prediction/components/make_pipeline.py
@@ -0,0 +1,33 @@
+import kfp
+from kfp.v2 import dsl,compiler
+from kfp.v2.dsl import component,pipeline,Output,Artifact,Input,Model
+
+pipeline_root_path          = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/kfp-json/'
+@dsl.pipeline(name          = "mlopsDeployPipeline-1",
+              pipeline_root = pipeline_root_path
+             )
+def mlopsDeployPipeline(project_in:str,
+                        endpoint_display_name_in:str,
+                        model_gcs_path_in:str,
+                        serving_container_in:str,
+                        bigquery_source_in:str,
+                        bigquery_destination_prefix_in:str,
+                        predictions_format_in:str,
+                        machine_type_in:str,
+                        bucket_uri_in:str
+                       ):
+    from create_endpoint import create_endpoint
+    from register_model import register_model
+    from deploy_model import deploy_model
+    from batch_prediction import batch_prediction
+    from loadBigQueryTableFromGCS import loadBigQueryTableFromGCS
+    
+    endpoint_op = create_endpoint(endpoint_display_name_in,project_in)
+    
+    model_op = register_model(project_in,endpoint_display_name_in,model_gcs_path_in,serving_container_in)
+
+    deploy_model(project_in,endpoint_display_name_in,model_op.output,endpoint_op.output)
+    
+    loadBigQueryTableFromGCS(project_in,'us-central1',bucket_uri_in)
+    
+    batch_prediction(project_in,endpoint_display_name_in,model_op.output,bigquery_source_in,bigquery_destination_prefix_in,predictions_format_in,machine_type_in)
