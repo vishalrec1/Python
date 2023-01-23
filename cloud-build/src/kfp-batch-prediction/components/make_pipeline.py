32569f0213ba0b4a17eb8a77b6027ea66ed8a05e
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-22 21:37:41 -0500

    Update make_pipeline.py

--- cloud-build/src/kfp-batch-prediction/components/make_pipeline.py
+++ cloud-build/src/kfp-batch-prediction/components/make_pipeline.py
@@ -3,7 +3,7 @@ from kfp.v2 import dsl,compiler
 from kfp.v2.dsl import component,pipeline,Output,Artifact,Input,Model
 
 pipeline_root_path          = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/kfp-json/'
-@dsl.pipeline(name          = "mlopsDeployPipeline-1",
+@dsl.pipeline(name          = "mlopsdeploypipeline-1",
               pipeline_root = pipeline_root_path
              )
 def mlopsDeployPipeline(project_in:str,
