696b3eb505929b898a6a85c9b5acba42cf799859
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-22 22:06:15 -0500

    Update compile-pipeline.py

--- cloud-build/src/kfp-batch-prediction/components/compile-pipeline.py
+++ cloud-build/src/kfp-batch-prediction/components/compile-pipeline.py
@@ -5,5 +5,6 @@ from kfp.v2.dsl import component,pipeline,Output,Artifact,Input,Model
 from make_pipeline import mlopsDeployPipeline
 
 compiler.Compiler().compile(pipeline_func = mlopsDeployPipeline,
-			    package_path  = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/kfp-json/mlopsdeploypipeline-1.json'
+			    #package_path  = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/kfp-json/mlopsdeploypipeline-1.json'
+			    package_path  =  '/mlopsdeploypipeline-1.json'
 						   )
