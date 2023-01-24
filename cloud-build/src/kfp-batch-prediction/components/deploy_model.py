77fcec7476bc03cf245f8b14863cb030776f83cc
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-23 19:26:05 -0500

    Update deploy_model.py

--- cloud-build/src/kfp-batch-prediction/components/deploy_model.py
+++ cloud-build/src/kfp-batch-prediction/components/deploy_model.py
@@ -6,7 +6,8 @@ from kfp.v2.dsl import component,pipeline,Output,Artifact,Input,Model
 CUSTOM_SERVING_CONTAINER  = "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-0:latest"
 @component(base_image            = CUSTOM_SERVING_CONTAINER,
            #output_component_file = 'gs://gcp-practice-0123-18jun2023/docker-kfp-test/yaml/deploy_model.yaml'
-           output_component_file = '/deploy_model.yaml'
+           output_component_file = '/deploy_model.yaml',
+           packages_to_install   = ['google-cloud-aiplatform']
           )
 def deploy_model(project_in:str,
                   model_display_name_in : str,
