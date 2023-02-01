7ace1b7d7e5631519b7668408292eee5ded89621
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-31 20:55:06 -0500

    Update upload_pipeline_template.py

--- cloud-build/src/kfp-batch-prediction/components-3/upload_pipeline_template.py
+++ cloud-build/src/kfp-batch-prediction/components-3/upload_pipeline_template.py
@@ -1,6 +1,6 @@
 from kfp.registry import RegistryClient
 
-client = RegistryClient(host=f"https://us-central1-kfp.pkg.dev/gcp-practice-0123/py-gar-repo-test")
+client = RegistryClient(host=f"https://us-central1-kfp.pkg.dev/gcp-practice-0123/kfp-repo1")
 templateName, versionName = client.upload_pipeline(
                                                    file_name="cloud-build/src/kfp-batch-prediction/components-3/mlopsdeploypipeline-1-31jan2023.yaml",
                                                    tags=["v1", "latest"],
