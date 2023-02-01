54d8a9c17700ee4b2b8259cb5c3dbe4d7be06a68
vishalrec1 <44909708+vishalrec1@users.noreply.github.com>
2023-01-31 20:36:10 -0500

    Update compile_pipeline.py

--- cloud-build/src/kfp-batch-prediction/components-3/compile_pipeline.py
+++ cloud-build/src/kfp-batch-prediction/components-3/compile_pipeline.py
@@ -3,7 +3,7 @@ from kfp.v2 import dsl, compiler
 from kfp.v2.dsl import component, pipeline, Output, Artifact, Input, Model
 from make_pipeline import mlopsDeployPipeline
 
-def compile_pipeline():
-    compiler.Compiler().compile(pipeline_func=mlopsDeployPipeline,
-                                package_path='cloud-build/src/kfp-batch-prediction/components-3/mlopsdeploypipeline-1-31jan2023.yaml'
-                                )
+#def compile_pipeline():
+compiler.Compiler().compile(pipeline_func=mlopsDeployPipeline,
+                            package_path='cloud-build/src/kfp-batch-prediction/components-3/mlopsdeploypipeline-1-31jan2023.yaml'
+                            )
